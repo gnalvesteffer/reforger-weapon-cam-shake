e10ffbfab8a9d19b00f9d52510815007de19f8df
ss-gnalvesteffer <xorberax@gmail.com>
2022-05-22 11:17:56 -0500

    cache player controller

--- scripts/Game/WeaponCameraShake.c
+++ scripts/Game/WeaponCameraShake.c
@@ -1,18 +1,20 @@
 modded class SCR_CharacterControllerComponent : CharacterControllerComponent
 {
 	IEntity m_Owner;
+	PlayerController m_PlayerController;
 	EventHandlerManagerComponent m_EventHandlerManager = null;
 
 	override void OnInit(IEntity owner)
 	{
 		m_Owner = owner;
+		m_PlayerController = GetGame().GetPlayerController();
 		m_EventHandlerManager = EventHandlerManagerComponent.Cast(owner.FindComponent(EventHandlerManagerComponent));
 		m_EventHandlerManager.RegisterScriptHandler("OnAmmoCountChanged", this, OnAmmoCountChanged, true);
 	}
 
 	private void OnAmmoCountChanged(BaseWeaponComponent weapon, BaseMuzzleComponent muzzle, BaseMagazineComponent magazine, int ammoCount, bool isBarrelChambered)
 	{
-		IEntity player = GetGame().GetPlayerController().GetControlledEntity();
+		IEntity player = m_PlayerController.GetControlledEntity();
 		if (player == m_Owner)
 		{
 			AddCameraShake();
